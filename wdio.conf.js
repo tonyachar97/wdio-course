e1916ac73a82124e38596fba696f90dc74e02a48
tonyachar97 <160266068+tonyachar97@users.noreply.github.com>
2024-02-18 23:01:19 -0800

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -137,7 +137,23 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['browserstack'],
+    services: [
+        ['selenium-standalone', {
+            logPath: 'logs',
+            installArgs: {
+                drivers: {
+                    chrome: { version: '121.0.6167.184' },
+                    firefox: { version: '122.0.1' }
+                }
+            },
+            args: {
+                drivers: {
+                    chrome: { version: '121.0.6167.184' },
+                    firefox: { version: '122.0.1' }
+                }
+            },
+        }]
+    ],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
